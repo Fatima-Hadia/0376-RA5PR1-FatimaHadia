74996041bc1c1d9b4a6faafd372288d21aaf44e5
Hadia Fatima <hfatima25@ilb.cat>
2026-03-25 12:27:28 +0100

    Table funcion

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+ <table>
+    <?php 
+  $numero = 7;
+      for  ($i = 1 to $i <= 10 $i++) { 
+        $resultat = $i * $numero 
+        echo "<tr>";     //←Table
+        echo "<td>$i x $numero</td>";  //← To show 1 x 7
+        echo "<td>= $resultat</td>";   //← To show = 7
+        echo "</tr>";
+}
+
+  ?>
+</table> 
+<?php
